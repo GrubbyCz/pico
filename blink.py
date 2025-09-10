1b350a2ce669d47d9bd934522cac693f6bffdee9
Adam Němeček <nemeceka22@spstrutnov.cz>
2025-09-10 08:15:09 +0200

    základ

---
+++ blink.py
@@ -0,0 +1,20 @@
+from machine import Pin
+from utime import sleep
+
+pin = Pin("LED", Pin.OUT)
+
+print("LED starts flashing...")
+while True:
+    try:
+        pin.toggle()
+        sleep(1) # sleep 1sec
+    except KeyboardInterrupt:
+        break
+pin.off()
+print("Finished.")
+dgdgdgrdg 
+dr globaldg 
+dg
+ 
+ g ertd
+ g 
